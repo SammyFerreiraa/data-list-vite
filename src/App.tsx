cd601ce3c51f238d8fd13f70cc264af22c24e607
Sammy Ferreira <sammyfe2021@gmail.com>
2024-03-07 08:34:15 -0300

    feat: search component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import { Plus } from 'lucide-react'
+import { Plus, Search, FileDown } from 'lucide-react'
 import { Header } from './components/header'
 import { Tabs } from './components/tabs'
+import { Button } from './components/ui/button'
+import { Control, Input } from './components/ui/input'
 export function App() {
   return (
     <div className="py-10 space-y-8">
@@ -11,10 +13,20 @@ export function App() {
       <main className="max-w-6xl mx-auto space-y-5">
         <div className="flex items-center gap-3">
           <h1 className="text-xl font-bold">Tags</h1>
-          <button className="inline-flex items-center gap-1.5 text-xs bg-teal-300 text-teal-950 font-medium rounded-full px-2 py-1">
+          <Button variant="primary">
             <Plus className="size-3 " />
             Create new
-          </button>
+          </Button>
+        </div>
+        <div className="flex items-center justify-between">
+          <Input variant="filter">
+            <Search className="size-3" />
+            <Control placeholder="Search tags..." />
+          </Input>
+          <Button>
+            <FileDown className="size-3" />
+            Export
+          </Button>
         </div>
       </main>
     </div>
