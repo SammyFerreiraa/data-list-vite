11e9be01f575b30f8a7b6ff6dd00acf2bccd22a2
Sammy Ferreira <sammyfe2021@gmail.com>
2024-03-07 09:25:23 -0300

    feat: table and navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,52 @@
-import { Plus, Search, FileDown } from 'lucide-react'
+import { Plus, Search, FileDown, MoreHorizontal } from 'lucide-react'
 import { Header } from './components/header'
 import { Tabs } from './components/tabs'
 import { Button } from './components/ui/button'
 import { Control, Input } from './components/ui/input'
+import { useQuery } from '@tanstack/react-query'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from './components/ui/table'
+import { Pagination } from './components/pagination'
+
+export interface Tags {
+  title: string
+  slug: string
+  amountOfVideos: number
+  id: string
+}
+
+export interface TagResponse {
+  first: number
+  prev: number | null
+  next: number
+  last: number
+  pages: number
+  items: number
+  data: Tags[]
+}
+
 export function App() {
+  const { data: tagsResponse, isLoading } = useQuery<TagResponse>({
+    queryKey: ['get-tags'],
+    queryFn: async () => {
+      const response = await fetch(
+        'http://localhost:3333/tags?_page=1&_per_page=10',
+      )
+      const data = await response.json()
+
+      return data
+    },
+  })
+
+  if (isLoading) {
+    return null
+  }
   return (
     <div className="py-10 space-y-8">
       <div>
@@ -28,6 +71,45 @@ export function App() {
             Export
           </Button>
         </div>
+
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead></TableHead>
+              <TableHead>Tag</TableHead>
+              <TableHead>Amount of videos</TableHead>
+              <TableHead></TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {tagsResponse?.data.map((tag) => (
+              <TableRow key={tag.id}>
+                <TableCell></TableCell>
+                <TableCell>
+                  <div className="flex flex-col gap-0.5">
+                    <span className="font-medium">{tag.title}</span>
+                    <span className="text-xs text-zinc-500">{tag.id}</span>
+                  </div>
+                </TableCell>
+                <TableCell className="text-zinc-300">
+                  {tag.amountOfVideos} Video(s)
+                </TableCell>
+                <TableCell className="text-right">
+                  <Button size="icon">
+                    <MoreHorizontal className="size-4" />
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        {tagsResponse && (
+          <Pagination
+            page={1}
+            pages={tagsResponse.pages}
+            items={tagsResponse.items}
+          />
+        )}
       </main>
     </div>
   )
